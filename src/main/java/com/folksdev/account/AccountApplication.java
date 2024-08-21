1a9ffe1d6230aebce4dd3f6ef2a61558a54b1232
Esra Gürgen <123691131+esragurgen@users.noreply.github.com>
2024-08-21 16:53:40 +0300

    changes

--- src/main/java/com/folksdev/account/AccountApplication.java
+++ src/main/java/com/folksdev/account/AccountApplication.java
@@ -2,13 +2,17 @@ package com.folksdev.account;
 
 import com.folksdev.account.model.Customer;
 import com.folksdev.account.repository.CustomerRepository;
+import org.apache.logging.log4j.util.Supplier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import java.time.Clock;
 import java.util.HashSet;
+import java.util.UUID;
 
 import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
@@ -31,9 +35,35 @@ public class AccountApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String...args) {
+    public void run(String...args) throws Exception{
         Customer customer = customerRepository.save(new Customer( "",  "Huseyin",  "Beller", new HashSet<>()));
         System.out.println(customer);
     }
+
+    @ConditionalOnProperty(
+            prefix = "command.line.runner",
+            value = "enabled",
+            havingValue = "true",
+            matchIfMissing = true
+    )
+
+    @Component
+    public static class CommandLineTaskExecutor implements CommandLineRunner{
+        @Bean
+        public Clock clock(){
+            return Clock.systemUTC();
+        }
+
+        @Bean
+        public Supplier<UUID> uuidSupplier(){
+            return UUID::randomUUID;
+        }
+
+        @Override
+        public void run(String... args) throws Exception {
+
+        }
+    }
+
 
 }
--- src/test/java/com/folksdev/account/controller/AccountControllerTest.java
+++ src/test/java/com/folksdev/account/controller/AccountControllerTest.java
@@ -2,16 +2,24 @@ package com.folksdev.account.controller;
 
 import static org.junit.runner.RunWith;
 
+import com.folksdev.account.dto.AccountDtoConverter;
+import com.folksdev.account.repository.AccountRepository;
+import com.folksdev.account.service.AccountService;
+import com.folksdev.account.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMve;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.function.Supplier;
+import java.util.UUID;
+import java.time.Clock;
+import org.junit.runner.RunWith;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@ActiveProfiles
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
         "server-port=0",
@@ -21,5 +29,28 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext
 
 class AccountControllerTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Clock clock;
+
+    @MockBean
+    private Supplier<UUID> uuidSupplier;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private AccountDtoConverter accountDtoConverter;
+
+    @Autowired
+    private AccountService service = new AccountService(accountRepository, customerService, converter);
+
+
 
 }
